4617df02afe75c70cf89615cf7d838446d217800
yonierva <yoniervasquez98@gmail.com>
2025-03-30 19:49:22 -0500

    cambio

--- src/main/java/com/proyectodeaula/proyecto_de_aula/ProyectoAulaApplication.java
+++ src/main/java/com/proyectodeaula/proyecto_de_aula/ProyectoAulaApplication.java
@@ -2,6 +2,7 @@ package com.proyectodeaula.proyecto_de_aula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.proyectodeaula")
